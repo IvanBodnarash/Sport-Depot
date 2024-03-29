a99f49bedff81cd65df48772e761abb303aed6a9
Ivan Bodnarash <bodnarash1999@gmail.com>
2024-03-29 11:22:44 -0600

    setting all the theme files

---
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+// Custom class for light & dark themes
+
+class SCheckboxTheme {
+  SCheckboxTheme._();
+
+  // Light Theme
+  static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateColor.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.orange;
+      } else {
+        return Colors.transparent;
+      }
+    })
+  );
+
+  // Dark Theme
+  static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateColor.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.orange;
+      } else {
+        return Colors.transparent;
+      }
+    })
+  );
+}
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:sport_depot/utils/theme/custom_themes/text_theme.dart';
-import 'package:sport_depot/utils/theme/custom_themes/elevated_button_theme.dart';
+
+import 'custom_themes/appbar_theme.dart';
+import 'custom_themes/bottom_sheet_theme.dart';
+import 'custom_themes/checkbox_theme.dart';
+import 'custom_themes/chip_theme.dart';
+import 'custom_themes/elevated_button_theme.dart';
+import 'custom_themes/outlined_button_theme.dart';
+import 'custom_themes/text_field_theme.dart';
+import 'custom_themes/text_theme.dart';
 
 class SAppTheme {
   SAppTheme._();
 
+  // Light Theme
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.light,
     primaryColor: Colors.orange,
-    scaffoldBackgroundColor: Colors.white,
     textTheme: STextTheme.lightTextTheme,
+    chipTheme: SChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: SAppBarTheme.lightAppBarTheme,
+    checkboxTheme: SCheckboxTheme.lightCheckboxTheme,
+    bottomSheetTheme: SBottomSheetTheme.lightBottomSheetTheme,
     elevatedButtonTheme: SElevatedButtonThemeData.lightElevatedButtonTheme,
+    outlinedButtonTheme: SOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: STextFormFieldTheme.lightInputDecorationTheme,
   );
+
+  // Dark Theme
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.dark,
     primaryColor: Colors.orange,
-    scaffoldBackgroundColor: Colors.black,
     textTheme: STextTheme.darkTextTheme,
+    chipTheme: SChipTheme.darkChipTheme,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: SAppBarTheme.darkAppBarTheme,
+    checkboxTheme: SCheckboxTheme.darkCheckboxTheme,
+    bottomSheetTheme: SBottomSheetTheme.darkBottomSheetTheme,
     elevatedButtonTheme: SElevatedButtonThemeData.darkElevatedButtonTheme,
+    outlinedButtonTheme: SOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: STextFormFieldTheme.darkInputDecorationTheme,
   );
 }
