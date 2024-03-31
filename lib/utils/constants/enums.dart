8ed90079909c2cf89999ffc78af34c7d251ebff2
Ivan Bodnarash <bodnarash1999@gmail.com>
2024-03-30 23:53:07 -0600

    configuring device utility nad setting sizes

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,7 @@
+// List of enums
+
+enum TextLSizes { small, medium, large }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethods { paypal, googlePay, visa, masterCard, creditCard, paystack }
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,10 @@
+class SImages {
+
+  // App logos
+  static const String darkAppLogo = 'assets\icons\brands';
+  static const String lightAppLogo = 'assets\icons\brands';
+
+  // Social logos
+    static const String google = 'assets\icons\brands';
+  static const String facebook = 'assets\icons\brands';
+}
